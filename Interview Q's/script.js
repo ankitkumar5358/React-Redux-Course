1d3a87e4dcaac00a3832d885739ea1495ccbcc49
ankitkumar5358 <createrankit@gmail.com>
2022-07-17 13:12:26 +0530

    ztm react mastery course codes

---
+++ Interview Q's/script.js
@@ -0,0 +1,17 @@
+// document.querySelector("body").innerText = "Hello World";
+
+function abc() {
+      //console.log(a);
+      console.log(b);
+      //console.log(c);
+      
+      // let a = 10;
+
+      var b = 20;
+      // const c = 30;
+      b = 30;
+      console.log(b);
+
+}
+
+abc();
